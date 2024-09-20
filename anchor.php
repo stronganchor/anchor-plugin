ed0f416d8d72cdea89f646e14cfd57a12378389c
stronganchor <77190959+stronganchor@users.noreply.github.com>
2024-09-20 10:20:51 +0300

    Create anchor.php

---
+++ anchor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Anchor
+ * Plugin URI: https://stronganchortech.com
+ * Description: Custom tools for managing Strong Anchor Tech's WordPress sites
+ * Author: Strong Anchor Tech
+ * Version: 0.0.0
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Include the plugin update checker
+require_once plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/stronganchor/anchor-plugin', // GitHub repository URL
+    __FILE__,                                                  // Full path to the main plugin file
+    'anchor-plugin'                                   // Plugin slug
+);
+
+// Set the branch to "main"
+$myUpdateChecker->setBranch('main');
+
+// Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
