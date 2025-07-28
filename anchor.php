e49450f09b21dd8056f185e3704c565817f8016e
stronganchor <77190959+stronganchor@users.noreply.github.com>
2025-07-28 21:00:41 +0300

    Update anchor.php

--- anchor.php
+++ anchor.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://stronganchortech.com
  * Description: Custom tools for managing Strong Anchor Tech's WordPress sites
  * Author: Strong Anchor Tech
- * Version: 1.1.0
+ * Version: 1.1.1
  */
 
 // Exit if accessed directly.
